f507c158ff11065a0c533dd01573b2028cb70f4c
MuhammadSaadHaroon <saadharoon6800@gmail.com>
2025-09-16 22:41:44 +0500

    correction

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.getElementById("submitBtn").addEventListener("click", function () {
   let name = document.getElementById("name").value;
-  let Class = document.getElementById("class").value;
-  let rollNo = document.getElementById("roll-no").value;
+  let studentClass = document.getElementById("class").value;
+  let roll = document.getElementById("roll").value;
 
   let subjects = [
     { sno: 1, name: "Computer", marks: 100, obtained: 85 },
-    { sno: 2, name: "Englissh", marks: 100, obtained: 75 },
+    { sno: 2, name: "English", marks: 100, obtained: 75 },
     { sno: 3, name: "Math", marks: 100, obtained: 90 }
   ];
 
@@ -20,41 +20,31 @@ document.getElementById("submitBtn").addEventListener("click", function () {
   else if (percentage >= 50) grade = "C";
   else grade = "Fail";
 
-  let newWindow = window.open("", "_blank");
-  newWindow.document.write(`
-    <html>
-      <head>
-        <title>Marksheet</title>
-        <link rel="stylesheet" href="css/style.css">
-      </head>
-      <body>
-        <h2>Student Marksheet</h2>
-        <p><b>Name:</b> ${name}</p>
-        <p><b>Class:</b> ${studentClass}</p>
-        <p><b>Roll No:</b> ${roll}</p>
+  document.getElementById("result").innerHTML = `
+    <h2>Student Marksheet</h2>
+    <p><b>Name:</b> ${name}</p>
+    <p><b>Class:</b> ${studentClass}</p>
+    <p><b>Roll No:</b> ${roll}</p>
 
-        <table border="1" cellspacing="0" cellpadding="8">
-          <tr>
-            <th>S.No</th>
-            <th>Subject</th>
-            <th>Total Marks</th>
-            <th>Obtained</th>
-          </tr>
-          ${subjects.map(s => `
-            <tr>
-              <td>${s.sno}</td>
-              <td>${s.name}</td>
-              <td>${s.marks}</td>
-              <td>${s.obtained}</td>
-            </tr>
-          `).join("")}
-        </table>
+    <table border="1" cellspacing="0" cellpadding="8">
+      <tr>
+        <th>S.No</th>
+        <th>Subject</th>
+        <th>Total Marks</th>
+        <th>Obtained</th>
+      </tr>
+      ${subjects.map(s => `
+        <tr>
+          <td>${s.sno}</td>
+          <td>${s.name}</td>
+          <td>${s.marks}</td>
+          <td>${s.obtained}</td>
+        </tr>
+      `).join("")}
+    </table>
 
-        <h3>Total Obtained: ${totalObtained} / ${totalMarks}</h3>
-        <h3>Percentage: ${percentage}%</h3>
-        <h3>Grade: ${grade}</h3>
-      </body>
-    </html>
-  `);
-  newWindow.document.close();
+    <h3>Total Obtained: ${totalObtained} / ${totalMarks}</h3>
+    <h3>Percentage: ${percentage}%</h3>
+    <h3>Grade: ${grade}</h3>
+  `;
 });
